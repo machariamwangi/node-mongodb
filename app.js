3f0c83d16c884d089a80ef0b801502d33e26ac90
machariamwangi <elijahmacharia54@gmail.com>
2020-08-14 23:16:56 +0300

    Creating test harness

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const dbName = 'circulation'
+const assert = require('assert');
+
 
 const circulationRepo = require('./repos/circulationRepo')
 const data = require('./circulation.json');
+
+ const url = 'mongodb://localhost:27017';
+    const dbName = 'circulation'
 
 async function  main() {
      const client = new MongoClient(url)
      await client.connect();
      
      const results = await circulationRepo.loadData(data)
-     console.log(results.insertedCount, results.ops);
+     assert.equal(data.length, results.insertedCount);
+    //  console.log(results.insertedCount, results.ops);
      const admin = client.db(dbName).admin()
+     await client.db(dbName).dropDatabase() 
     //  console.log(await admin.serverStatus());
      console.log(await admin.listDatabases());
+     client.close()
 
 }
 main();
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -13,6 +13,7 @@ function circulationRepo() {
 
                   results  = await db.collection('newspapers').insertMany(data)
                   resolve(results)
+                  client.close()
 
                }catch(error){
                    reject(error)
